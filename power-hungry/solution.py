777f0ee9ca89fe39db79f8e836342514874a7c6a
hungnq <hungnq@sendo.vn>
2021-08-12 17:23:25 +0700

    init source

---
+++ power-hungry/solution.py
@@ -0,0 +1,3 @@
+def solution(xs):
+    #code here
+    
---
+++ power-hungry/test_solution.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+import unittest
+from solution import *
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        self.assertAlmostEqual(solution([2, 0, 2, 2, 0]),8)
+        self.assertAlmostEqual(solution([-2, -3, 4, -5]),60)
+        self.assertAlmostEqual(solution([2, 0]),2)
+        self.assertAlmostEqual(solution([0,-1, -10]),10)
+        self.assertAlmostEqual(solution([0,-1, 0, -10, 2, 5]),100)
+        self.assertAlmostEqual(solution([0]),0)
+        self.assertAlmostEqual(solution([0]),0)
+        
+
+if __name__ == '__main__':
+    unittest.main()
